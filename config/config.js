d028fd7ee9707c05b61c5c44ad75d82d2b5db166
Jesse Paris <jparis@gmail.com>
2018-08-07 12:15:25 -0700

    Revert "New list view page for iPortal users"
    This reverts commit 61aec2283971cf340a05bfda888034bc56603e81.

--- config/config.js
+++ config/config.js
@@ -12,19 +12,11 @@ var csvFilePath = path.join(__dirname, '/../udc/');
 var logFile = logPath + 'iPortal_' + dateTimeString() + '.log';
 
 var config = extend(true, {
-    defaultView: 'list',
-    showCorporateGroups: false,
-    showQlikGroups: false,
-    showLicenseGroup: false,
-    showDescriptionColumn: false,
-    showCorporateColumn: true,
-    showQlikColumn: true,
-    showLicenseColumn: false,
-    serverPort: 3091,
+    serverPort: 3090,
     qpsPort: 4243,
     qrsPort: 4242,
     repoAccount: 'UserDirectory=Internal;UserId=sa_repository',
-    hostname: 'localhost',
+    hostname: 'senseServerName',
     virtualProxy: 'iportal',
     allowedConnections: 'domainOfSystems',
     userDirectory: 'iportal',
--- routes/index.js
+++ routes/index.js
@@ -19,22 +19,12 @@ var logger = new(winston.Logger)({
 	GET /
 
 	This is the only web page in this application and it displays the list of users
-	associated with the CSV UDS
+	associated with the EXCELUDS
 
  */
 router.get('/', function(req, res, next) {
     logger.debug('Route: GET /');
 
-    if (cfg.defaultView == 'grid') {
-        res.redirect('/grid');    
-    } else {
-        res.redirect('/list');    
-    }
-});
-
-router.get('/list', function(req, res, next) {
-    logger.debug('Route: GET /list');
-
     handlebars.registerHelper('ButtonStyle', function(app) {
         if (app.toUpperCase() == "HUB") {
             return "btn-success";
@@ -46,24 +36,7 @@ router.get('/list', function(req, res, next) {
     var config = users.loadUsers("iportal_users.csv", "iportal_attributes.csv");
     config.cfg = cfg;
 
-    res.render('list', config);
-});
-
-router.get('/grid', function(req, res, next) {
-    logger.debug('Route: GET /grid');
-
-    handlebars.registerHelper('ButtonStyle', function(app) {
-        if (app.toUpperCase() == "HUB") {
-            return "btn-success";
-        } else {
-            return "btn-primary";
-        }
-    });
-
-    var config = users.loadUsers("iportal_users.csv", "iportal_attributes.csv");
-    config.cfg = cfg;
-
-    res.render('grid', config);
+    res.render('index', config);
 });
 
 router.get('/login', function(req, res, next) {
